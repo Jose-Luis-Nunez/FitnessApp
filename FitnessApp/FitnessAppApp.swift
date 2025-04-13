5036f1f13e9100942328ab943cda45c11eb6bdc1
josenunez <josenunez@outlook.de>
2025-04-13 12:43:50 +0200

    Initial Commit

---
+++ FitnessApp/FitnessAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FitnessAppApp.swift
+//  FitnessApp
+//
+//  Created by Jose Nunez on 13.04.25.
+//
+
+import SwiftUI
+
+@main
+struct FitnessAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ FitnessAppTests/FitnessAppTests.swift
@@ -0,0 +1,16 @@
+//
+//  FitnessAppTests.swift
+//  FitnessAppTests
+//
+//  Created by Jose Nunez on 13.04.25.
+//
+
+import Testing
+
+struct FitnessAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
